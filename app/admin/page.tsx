9dfdaf925a51c9b9164e232a4a88548a17f5b827
tranngochung29200333333-commits <tranngochung29200333333@gmail.com>
2026-09-18 07:50:17 +0700

    feat: add admin listing moderation

---
+++ app/admin/page.tsx
@@ -0,0 +1,2 @@
+import Header from "../../components/Header";import AdminClient from "./AdminClient";
+export default function AdminPage(){return <main><Header/><section className="container-page py-10"><AdminClient/></section></main>}
