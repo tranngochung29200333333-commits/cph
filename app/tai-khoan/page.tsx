fa94a228edb29661c3f2aa4eea5d1485a2c645de
tranngochung29200333333-commits <tranngochung29200333333@gmail.com>
2026-09-18 08:11:13 +0700

    Giữ trang tài khoản

--- app/tai-khoan/page.tsx
+++ app/tai-khoan/page.tsx
@@ -1,2 +1,1 @@
-import Header from "../../components/Header";import AccountClient from "./AccountClient";
-export default function AccountPage(){return <main><Header/><section className="container-page py-10"><AccountClient/></section></main>}
+import Header from "../../components/Header";import AccountClient from "./AccountClient";export default function AccountPage(){return <main><Header/><section className="container-page py-10"><AccountClient/></section></main>}
