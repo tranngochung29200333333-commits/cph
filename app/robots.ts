891cda6167c536f63bdeb377f5086d5910623d80
tranngochung29200333333-commits <tranngochung29200333333@gmail.com>
2026-09-18 09:19:24 +0700

    feat: hoàn thiện auth SEO và trang 404

---
+++ app/robots.ts
@@ -0,0 +1,2 @@
+import type {MetadataRoute} from "next";
+export default function robots():MetadataRoute.Robots{return{rules:{userAgent:"*",allow:"/"},sitemap:"https://tranngochung29200333333-commits.github.io/cph/sitemap.xml"}}
