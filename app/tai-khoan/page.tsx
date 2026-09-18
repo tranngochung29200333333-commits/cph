85e1de068c78656a1f13eee0a0747ba5d9c912e7
tranngochung29200333333-commits <tranngochung29200333333@gmail.com>
2026-09-18 07:49:50 +0700

    feat: add seller dashboard and browse pages

---
+++ app/tai-khoan/page.tsx
@@ -0,0 +1,2 @@
+import Header from "../../components/Header";import AccountClient from "./AccountClient";
+export default function AccountPage(){return <main><Header/><section className="container-page py-10"><AccountClient/></section></main>}
